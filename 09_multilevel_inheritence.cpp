399f628db8c388cb42ac383b4a1bdab3c4defda5
atharvsankpal7 <atharvsankpal69420@gmail.com>
2022-12-06 19:34:36 +0530

    1

--- 09_multilevel_inheritence.cpp
+++ 09_multilevel_inheritence.cpp
@@ -1,61 +1,52 @@
 #include <iostream>
 using namespace std;
-class student_info
-{
+class student_info {
 protected:
-    string name;
-    int URN;
+  string name;
+  int URN;
 
 public:
-    void getinfo()
-    {
-        cout << "Enter the name of student --> " << endl;
-        cin >> name;
-        cout << "Enter the URN of student --> " << endl;
-        cin >> URN;
-    }
+  void getinfo() {
+    cout << "Enter the name of student --> " << endl;
+    getline(cin, name);
+    cout << "Enter the URN of student --> " << endl;
+    cin >> URN;
+  }
 };
-class marks : public student_info
-{
+class marks : public student_info {
 protected:
-    int sub[5];
+  int sub[5];
 
 public:
-    void getmarks()
-    {
-        cout << "Enter the marks of student --> ";
-        for (int i = 0; i < 5; i++)
-        {
-            cout << "Enter the marks of subject " << i + 1 << " ";
-            cin >> sub[i];
-        }
+  void getmarks() {
+    cout << "Enter the marks of student --> ";
+    for (int i = 0; i < 5; i++) {
+      cout << "Enter the marks of subject " << i + 1 << " ";
+      cin >> sub[i];
     }
+  }
 };
-class CGPA : public marks
-{
+class CGPA : public marks {
 private:
-    float avg = 0;
+  float avg = 0;
 
 public:
-    void calculateavg()
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            avg += sub[i];
-        }
-        avg /= 5.0;
+  void calculateavg() {
+    for (int i = 0; i < 5; i++) {
+      avg += sub[i];
     }
-    void DisplayData()
-    {
-        cout << "The CGPA of the student named " << name << " with URN " << URN << " is " << avg << endl;
-    }
+    avg /= 5.0;
+  }
+  void DisplayData() {
+    cout << "The CGPA of the student named " << name << " with URN " << URN
+         << " is " << avg << endl;
+  }
 };
-int main()
-{
-    CGPA s1;
-    s1.getinfo();
-    s1.getmarks();
-    s1.calculateavg();
-    s1.DisplayData();
-    return 0;
+int main() {
+  CGPA s1;
+  s1.getinfo();
+  s1.getmarks();
+  s1.calculateavg();
+  s1.DisplayData();
+  return 0;
 }
