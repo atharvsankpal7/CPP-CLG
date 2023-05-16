d8c1a961157a29b53b6468c6cae47a52572faf94
atharvsankpal7 <atharvsankpal69420@gmail.com>
2023-05-16 11:23:56 +0530

    optimize

--- 02_bank_system_using_constructor_and_destructor.cpp
+++ 02_bank_system_using_constructor_and_destructor.cpp
@@ -1,41 +1,51 @@
 #include <iostream>
 using namespace std;
-class account {
+class account
+{
 private:
   float bal = 0, intr, amt, rate, dep, month;
   long int acc;
   string name;
 
 public:
-  void display() { cout << "The balance in the account is " << bal << endl; }
-  void withdraw() {
+  void display()
+  {
+    cout << "The balance in the account is " << bal << endl;
+  }
+  void withdraw()
+  {
     int a;
     cout << "Enter the amount of money that you want to withdraw-->\n";
     cin >> a;
     bal -= a;
     display();
   }
-  void deposite() {
+  void deposite()
+  {
     int a;
     cout << "Enter the amount that you want to deposite -->" << endl;
     cin >> a;
     bal += a;
     display();
   }
-  account() {
+  account()
+  {
     cout << "Enter the account no-->" << endl;
     cin >> acc;
     cout << "Enter the name of account holder-->" << endl;
     cin >> name;
   }
-  ~account() { cout << "Thanks for visiting!!!\n"; }
-  void menu() {
+  ~account()
+  {
+    cout << "Thanks for visiting!!!\n";
+  }
+  void menu()
+  {
     int a;
-    cout << "\n1.Display Balance \n2.Deposite Money \n3.Withdraw "
-            "\n4.Stop"
-         << endl;
+    cout << "\n1.Display Balance \n2.Deposite Money \n3.Withdraw \n4.Stop" << endl;
     cin >> a;
-    switch (a) {
+    switch (a)
+    {
     case 1:
       display();
       break;
@@ -50,12 +60,12 @@ public:
 
     default:
       return;
-      break;
     }
     menu();
   }
 };
-int main() {
+int main()
+{
   account a1;
   a1.menu();
   return 0;
